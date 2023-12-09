28f3eaefe8bb34e612919e4e6ae47779c5708853
Ron <ron062394@gmail.com>
2023-12-09 11:13:52 +0800

    Added looping statement

---
+++ 5-JavaScript/8-looping-statements.js
@@ -0,0 +1,21 @@
+//Looping statement
+console.log("i am connected")
+
+//For loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Count:", i);
+}
+
+// Loops: while loop
+let count = 1;
+while (count <= 5) {
+  console.log('Count:', count);
+  count++;
+}
+
+// Loops: do-while loop
+let i = 1;
+do {
+  console.log('Do-While Count:', i);
+  i++;
+} while (i <= 5);
